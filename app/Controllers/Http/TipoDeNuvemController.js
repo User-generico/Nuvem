83d1f61a80ed17b8824a8111e8fe2bb45bcb50af
Isis S. P <emailgenerico742@gmail.com>
2020-12-03 18:15:31 -0300

    comitandoo

--- app/Controllers/Http/TipoDeNuvemController.js
+++ app/Controllers/Http/TipoDeNuvemController.js
@@ -3,6 +3,7 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+const Tipo = use("app/Models/TipoDeNuvem") 
 
 /**
  * Resourceful controller for interacting with tipodenuvems
@@ -21,29 +22,6 @@ class TipoDeNuvemController {
   }
 
   /**
-   * Render a form to be used for creating a new tipodenuvem.
-   * GET tipodenuvems/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
-
-  /**
-   * Create/save a new tipodenuvem.
-   * POST tipodenuvems
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
-  }
-
-  /**
    * Display a single tipodenuvem.
    * GET tipodenuvems/:id
    *
@@ -53,40 +31,8 @@ class TipoDeNuvemController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing tipodenuvem.
-   * GET tipodenuvems/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update tipodenuvem details.
-   * PUT or PATCH tipodenuvems/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a tipodenuvem with id.
-   * DELETE tipodenuvems/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
+    const tipos = await Tipo.all();
+    return tipos;
   }
 }
 
